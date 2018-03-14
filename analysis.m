32209a5940a1ab59675c6be910133b2da185dac0
rozoalex <alexhubnds@icloud.com>
2018-03-14 14:14:30 -0400

    error so big

--- analysis.m
+++ analysis.m
@@ -1,11 +1,15 @@
 % 
 clear; 
+number_of_train = 250;
 %% Load Training data
-train_data = [importdata('hw4.train1');importdata('hw4.train2');importdata('hw4.train3')];
+train_data = importdata('hw4.train1');
+% importdata('hw4.train2');importdata('hw4.train3')
 ANS = train_data(:,1);
-FEATURES = train_data(:,2:end);
+train_FEATURES = train_data(1:number_of_train,2:end);
+train_ANS = train_data(1:number_of_train,:,1);
+train_ANS = train_ANS(:,1);
 %% PCA and Plot the Variances
-[coefs,scores,variances] = pca(FEATURES');
+[coefs,scores,variances] = pca(train_FEATURES);
 normalized_variances = variances / variances(1);
 % Drawing time
 var_graph = figure('Name', 'Variances of PCA');
@@ -27,53 +31,68 @@ title('Normalized Variances for the first 4 biggest ')
 annotation('textbox', [.15, .05, .7, .15], 'string', 'We can see that, the first 2 principle components are so huge compare to others, which means the result is mostly dependent on those two pcs. Close the window to continue...')
 % waitfor(var_graph) % close the window to continue
 %%
-f1 = scores(1,:)'; % feature 1
-f2 = scores(2,:)'; % feature 2
-f3 = scores(3,:)'; % feature 3
+pc1 = scores(:,1); % feature 1
+pc2 = scores(:,2); % feature 2
+pc3 = scores(:,3); % feature 3
 figure('Name', 'f1 f2 f3')
 %
 subplot(2,2,1)
-scatter3(f1,f2,ANS)
-xlabel('f1')
-ylabel('f2')
+scatter3(pc1,pc2,train_ANS(:,1),'.')
+xlabel('pc1')
+ylabel('pc2')
 zlabel('ans')
 %
 subplot(2,2,2)
-plot(f1,f2,'.')
-xlabel('f1')
-ylabel('f2')
+plot(pc1,pc2,'.')
+xlabel('pc1')
+ylabel('pc2')
 %
 subplot(2,2,3)
-plot(f1,ANS,'.')
-xlabel('f1')
+plot(pc1,train_ANS,'.')
+xlabel('pc1')
 ylabel('ans')
 %
 subplot(2,2,4)
-plot(f2,ANS,'.')
-xlabel('f2')
+plot(pc2,train_ANS,'.')
+xlabel('pc2')
 ylabel('ans')
 
 %% Linear Regression
 % With 2 features
-rgs_2 = regress(ANS,[ones(size(f1)) f1 f2]);
+rgs_2 = regress(train_ANS,[ones(size(pc1)) pc1 pc2]);
 % With 3 features
-rgs_3 = regress(ANS,[ones(size(f1)) f1 f2 f3]);
 figure('Name','Linear Regression')
-scatter3(f1,f2,ANS,'filled')
+scatter3(pc1,pc2,train_ANS,'filled')
 hold on
-f1fit = min(f1):100:max(f1);
-f2fit = min(f2):10:max(f2);
+
+
+
+f1fit = min(pc1):100:max(pc1);
+f2fit = min(pc2):10:max(pc2);
 [F1FIT,F2FIT] = meshgrid(f1fit,f2fit);
 YFIT = rgs_2(1) + rgs_2(2)*F1FIT + rgs_2(3)*F2FIT;
 mesh(F1FIT,F2FIT,YFIT)
-xlabel('f1')
-ylabel('f2')
+xlabel('pc1')
+ylabel('pc2')
 zlabel('ans')
-view(50,10)
+% view(50,10)
 
 %% Varify Errors
-% verification_data = [importdata('hw4.train4')];
-% test_data = [importdata('hw4.test1');importdata('hw4.test2');importdata('hw4.test3');importdata('hw4.test4')];
-
-
+varify_FEATURES = train_data(number_of_train+1:304,2:end);
+varify_ANS = ANS((number_of_train+1):304);
+varify_transd_FEATURES = varify_FEATURES * coefs;
+varify_pc1 = varify_transd_FEATURES(:,1);
+varify_pc2 = varify_transd_FEATURES(:,2);
+varify_pc3 = varify_transd_FEATURES(:,3);
+varify_pcs = [varify_pc1 varify_pc2 varify_pc3];
+varify_predict_ANS = varify_pcs * rgs_2;
+figure('Name','Varify Predicted Results')
+plot(varify_ANS, varify_predict_ANS,'.')
+xlabel('ans')
+ylabel('predict')
+RMSE = sqrt(mean((varify_ANS - varify_predict_ANS).^2));  % Root Mean Squared Error
+annotation('textbox', [.15, .05, .7, .15], 'string', sprintf('RMSE = %0.5e',RMSE))
 %%
+% 
+% l = len(train_ANS)
+% rgs_3 = regress(ANS,[ones(size(f1)) f1 f2 f3]);
